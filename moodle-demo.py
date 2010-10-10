c523ae650404004a4c9ef0e77eda584ea191d5d8
dave bl <db@d1b.org>
2010-10-10 20:12:27 +1100

    unhack the code base (don't use specific offsets etc.) download more than just pdf files use lxml to perform parsing check and get file names from the resource page
    Signed-off-by: dave bl <db@d1b.org>

--- moodle-demo.py
+++ moodle-demo.py
@@ -1,74 +1,69 @@
 #!/usr/bin/env python
 import urllib
-import urllib2
-import re
 import pycurl
 import StringIO
 import sys
 import os
 import getpass
-import BeautifulSoup
+from lxml import html
 
 __author__ = "dave bl. db@d1b.org"
-__version__= "0.2"
+__version__= "0.3"
 __license__= "gpl v2"
 __program__ = "FOS moodle schedule info / file downloader"
 
 def return_red(text):
-	return "\033[0;41m" +text+ "\033[m"
+	return "\033[0;41m" + text + "\033[m"
 
 def return_blue(text):
-	return "\033[0;44m" +text+ "\033[m"
+	return "\033[0;44m" + text + "\033[m"
 
 def return_green(text):
-	return "\033[01;42m" +text+ "\033[m"
+	return "\033[01;42m" + text + "\033[m"
 
-def strip_tags(value):
-	return re.sub(r'<[^>]*?>', '', value)
+def get_resources_from_resource_page(the_page, front_part_of_url="https://moodle.comp.mq.edu.au/mod/resource/"):
+	dict_res = {}
+	doc = html.fromstring(the_page)
+	for resource in doc.xpath("//td[@class='cell c1']/a"):
+		safe_name = replace_f_name_with_safer_version(resource.text)
+		dict_res[safe_name] = front_part_of_url + resource.attrib['href']
+	return dict_res
 
-def get_resource_pdf_links(the_page):
-	""" returns a list of pdf resource urls """
-	resource_list = []
-	for i in the_page.split("\n"):
-		if "/resource" in i and "PDF" in i:
-			index_offset = i.find("href=")
-			index_end = i.find('"><img src')
-			if index_end !=-1 and index_offset != -1:
-				res = i[index_offset+6:index_end]
-				resource_list.append(res)
-	return resource_list
+def get_alt_resource_link(the_page):
+	doc = html.fromstring(the_page)
+	for resource in doc.xpath("//div[@class='popupnotice']/a/@href"):
+		return resource
 
-def get_alt_resource_pdf_link(the_page):
-	""" returns the actual location of a pdf if the poster has put a 'preview page' in front of it """
-	for i in the_page.split("\n"):
-		if "resourcepdf" in i:
-			index_offset = i.find("href=")
-			index_end = i.find(""".pdf">""")
-			if index_end != -1 and index_offset != -1:
-				return i[index_offset+6:index_end+4]
+def replace_f_name_with_safer_version(name):
+	name = name.strip()
+	name = name.replace("/", "_")
+	name = name.replace(" ", "_")
+	name = name.replace("..", "_")
+	name = name.replace("\"", "_")
+	name = name.replace("~", "_")
+	name = name.replace('"', "_")
+	name = name.replace("'", "_")
+	name = name.replace(".py", "_python")
+	name = name[0:253]
+	return name
 
-def get_events(page):
-	events = []
+
+def get_events(the_page):
 	submited = []
 	not_submited = []
 	graded = []
-	soup = BeautifulSoup.BeautifulSoup(page)
-	for i in soup.findAll("div", { "class" :"assignment overview"} ):
-		event = strip_tags(str(i))
-		if "2010" in event:
-			events.append(event)
-	for i in events:
-		i = i.replace("PM","PM ")
-		i = i.replace("AM", "AM ")
-		i = i.replace("Due", " Due")
-		i = i.strip()
-		i = i + "\n"
-		if "not submitted" in i.lower():
-			 not_submited.append(return_red(i))
-		elif "graded" in i.lower() and "not graded" not in i.lower():
-			graded.append(return_green(i))
-		elif "submitted," in i.lower():
-			submited.append(return_blue(i))
+	doc = html.fromstring(the_page)
+
+	for assign in doc.xpath("//div[@class='assignment overview']"):
+		for name, date,in zip (assign.xpath("div[@class='name']/a/text()"), assign.xpath("div[@class='info']/text()")):
+			status = assign.xpath("text()")[0]
+			as_details = name + " " + date
+			if "not submitted" in status.lower():
+				not_submited.append(return_red(as_details))
+			elif "graded" in status.lower() and "not graded" not in status.lower():
+				graded.append(return_green(as_details))
+			elif "submitted," in status.lower():
+				submited.append(return_blue(as_details))
 	result = graded + submited + not_submited
 	return result
 
@@ -87,7 +82,7 @@ def get_user_credentials_from_user_input(conn_details):
 	conn_details["password"] = getpass.getpass("enter your password\n")
 	return conn_details
 
-def fetch_from_moodle(url_login, url_target, conn_details):
+def get_moodle(url_login, url_target, conn_details):
 	submit_data_t = [ ('username', str(conn_details["username"]) ), ('password', conn_details["password"])  ]
 	submit_data_t = urllib.urlencode(submit_data_t)
 	cookie_loc = os.path.expanduser("~/.mq/moodle_cookie")
@@ -103,48 +98,50 @@ def fetch_from_moodle(url_login, url_target, conn_details):
 	connection.setopt(pycurl.URL, url_login)
 	connection.perform()
 
+	#now get the page we want
+	string_s = StringIO.StringIO()
 	connection.setopt(pycurl.WRITEFUNCTION, string_s.write)
 	connection.setopt(pycurl.URL, url_target)
+	connection.setopt(pycurl.FOLLOWLOCATION, True)
 	connection.perform()
+
 	the_page = str(string_s.getvalue())
+	return the_page, connection
 
-	#follow resource PDF links should they exist (for a course)
-	if "course/" in url_target:
-		resource_list = get_resource_pdf_links(the_page)
-		try:
-			create_directory("/tmp/PDF")
-		except Exception, e:
-			print e
 
-		for i in resource_list:
+def create_tmp_dir(dir_path):
+	try:
+		create_directory(dir_path)
+	except Exception, e:
+		print e
+
+def download_resources_using_connection_from_resource_page(the_page, connection, dir_path="/tmp/moodle_dl"):
+	resources = get_resources_from_resource_page(the_page)
+	create_directory(dir_path)
+	for name, link in resources.items():
+		new_s = StringIO.StringIO()
+		connection.setopt(pycurl.FOLLOWLOCATION, True)
+		connection.setopt(pycurl.WRITEFUNCTION, new_s.write)
+		connection.setopt(pycurl.URL, link)
+		connection.perform()
+		temp_file = str(new_s.getvalue())
+		real_url = connection.getinfo(pycurl.EFFECTIVE_URL)
+		if "https://moodle.comp.mq.edu.au/mod/resource/view.php?id=" in real_url:
 			new_s = StringIO.StringIO()
-			connection.setopt(pycurl.FOLLOWLOCATION, True)
+			actual_res_loc = get_alt_resource_link(temp_file)
+			if actual_res_loc is None:
+				continue
+			connection.setopt(pycurl.FOLLOWLOCATION, False)
 			connection.setopt(pycurl.WRITEFUNCTION, new_s.write)
-			connection.setopt(pycurl.URL, i)
+			connection.setopt(pycurl.URL, actual_res_loc)
 			connection.perform()
 			temp_file = str(new_s.getvalue())
 			real_url = connection.getinfo(pycurl.EFFECTIVE_URL)
-			if "https://moodle.comp.mq.edu.au/mod/resource/view.php?id=" in real_url:
-				new_s = StringIO.StringIO()
-				actual_res_loc = get_alt_resource_pdf_link(temp_file)
-				connection.setopt(pycurl.FOLLOWLOCATION, False)
-				connection.setopt(pycurl.WRITEFUNCTION, new_s.write)
-				connection.setopt(pycurl.URL, actual_res_loc)
-				connection.perform()
-				temp_file = str(new_s.getvalue())
-				real_url = connection.getinfo(pycurl.EFFECTIVE_URL)
+		print real_url
 
-			print real_url
-			name_from_last_url = real_url.rfind("/")
-			res_name = real_url[name_from_last_url+1:]
-			full_file_loc = "/tmp/PDF/" + res_name
-
-			assert os.path.realpath(full_file_loc).startswith("/tmp/PDF/"), " assert that we are only going to write the file to /tmp/PDF/"
-			write_to_a_file(temp_file, full_file_loc)
-		connection.close()
-		return None
-	connection.close()
-	return the_page
+		full_file_loc = dir_path + "/" + replace_f_name_with_safer_version(name)
+		assert os.path.realpath(full_file_loc).startswith(dir_path + "/"), " assert that we are only going to write the file to the temp dir"
+		write_to_a_file(temp_file, full_file_loc)
 
 def write_to_a_file(data,full_file_loc):
 	the_file = open(full_file_loc, 'w')
@@ -189,37 +186,43 @@ def delete_cookie(folder_loc):
 	except Exception, e:
 		print e
 
+def get_auth_information(conn_details):
+	try:
+		conn_details = get_credentials_from_accounts_file(conn_details)
+	except Exception, e:
+		print e
+		create_directory(os.path.expanduser("~/.mq"))
+		conn_details = get_user_credentials_from_user_input(conn_details)
+
+		save_credentials_to_accounts_file(conn_details["username"], conn_details["password"], os.path.expanduser("~/.mq/moodle_ics_account") )
+	return conn_details
+
 def main():
 	conn_details = {}
 	url_login = "https://moodle.comp.mq.edu.au/login/index.php"
 	url_events = "https://moodle.comp.mq.edu.au/my/index.php"
-	url_course = "https://moodle.comp.mq.edu.au/course/view.php?id=52&week=0#section-1"
+	url_course = "https://moodle.comp.mq.edu.au/mod/resource/index.php?id=68"
 
-	try:
-		get_credentials_from_accounts_file(conn_details)
-	except Exception, e:
-		print e
-		create_directory(os.path.expanduser("~/.mq"))
-		get_user_credentials_from_user_input(conn_details)
-
-		save_credentials_to_accounts_file(conn_details["username"], conn_details["password"], os.path.expanduser("~/.mq/moodle_ics_account") )
+	conn_details = get_auth_information(conn_details)
 
 	print "enter dl for downloading course material otherwise press s for due dates etc."
 	choice = get_input()
 	if "dl" in choice:
-		print return_red("NOTE: the default course url is for COMP348 and will not work for other courses!")
-		print return_red("files will be downloaded to /tmp/PDF/")
+		print return_red("NOTE: the default course url is for COMP332 and will not work for other courses!")
+		print return_red("files will be downloaded to /tmp/moodle_dl/")
 		target_url = url_course
 	elif "s" in  choice:
 		target_url = url_events
 
-	page = fetch_from_moodle(url_login, target_url, conn_details)
-
-	if page != None:
-		events = get_events(page)
+	the_page, connection = get_moodle(url_login, target_url, conn_details)
+	if target_url == url_events:
+		events = get_events(the_page)
 		for i in events:
 			print i
+	else:
+		download_resources_using_connection_from_resource_page(the_page, connection)
 
+	connection.close()
 	delete_cookie(os.path.expanduser("~/.mq/") )
 
 if __name__=='__main__':
